de38e1df52deec8ac44fd1c9ed7e1bede6328e01
POOJABINJWA <poojasureshbinjwa21@navgurukul.org>
2024-08-30 17:49:09 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Navbar from "./Navbar";
 import MainContainer from './MainContainer';
 import Details from './details/Details.js';
 import Footer from './Footer.js';
+import About from './about/About.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Router>
+
         <Routes>
           <Route path="/" element={<MainContainer />} />
           <Route path="/details" element={<Details />} />
+          <Route path="/about" element={< About />}/>
         </Routes>
-      </Router>
+
       <Footer/>
     </>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,39 @@
-import "./footer.css"
-function Footer (){
-    return (
-        <div className="footer">footer</div>
-    )
+import "./footer.css";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footerDetails">
+                <div className="aboutUs">
+                    <h3>About Us</h3>
+                    <p>We are dedicated to the empowerment of farmers and promoting sustainability.</p>
+                    <Link to="/about">Learn More</Link>
+                </div>
+                <div className="contactUs">
+                    <h3>Contact Us</h3>
+                    <p>Phone: +123456789</p>
+                    <p>Email: support@farmerdonites.com</p>
+                    <Link to="/contact">Get in Touch</Link>
+                </div>
+                <div className="social-media-icons">
+                    <a href="https://www.facebook.com"  >
+                    <FaFacebook className="icon" />
+                    </a>
+                    <a href="https://www.twitter.com"  >
+                    <FaTwitter className="icon" />
+                    </a>
+                    <a href="https://www.linkedin.com" >
+                    <FaLinkedin className="icon" />
+                    </a>
+                    <a href="https://www.instagram.com"  >
+                    <FaInstagram className="icon" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 export default Footer;
