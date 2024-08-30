0886c981fd3977378bf6656803c7095a07ad3874
POOJABINJWA <poojasureshbinjwa21@navgurukul.org>
2024-08-30 13:54:15 +0530

    background image set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './index.css'
 import Navbar from "./Navbar";
+import MainContainer from './MainContainer';
 function App(){
   return (
     <>
       <Navbar/>
+      <MainContainer/>
     </>
   )
 }
---
+++ src/MainContainer.js
@@ -0,0 +1,14 @@
+import "./mainContainer.css"
+function MainContainer(){
+    return(
+        <div className="mainContainer">
+            <div className="containerDetails">
+                <img src="background.png" alt="" className="backgroundImage"/>
+                <div className="heading">Empowering Agriculture for a Sustainable Future</div>
+               
+            </div>
+        </div>
+    )
+
+}
+export default MainContainer;
