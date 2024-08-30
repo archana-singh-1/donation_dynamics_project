d67edd48545806f7f128de9e5aa0cfa43b5e35e9
POOJABINJWA <poojasureshbinjwa21@navgurukul.org>
2024-08-31 01:46:07 +0530

    fetch dummy data

--- src/Donair_details_col.js
+++ src/Donair_details_col.js
@@ -2,8 +2,10 @@ import "./donaier_details_col.css"
 function Donair_details_col({details}){
     return(
         <div className=" donair_details_col">
-              <img src={details.imgSrc} alt="" className="img_1"/>
-            <p>{details.title}</p>
+            <img src={details.imgSrc} alt="" className="img_1"/>
+            <p className="title">{details.title}</p>
+            <p className="details">{details.description}</p>
+
         </div>
     )
 
