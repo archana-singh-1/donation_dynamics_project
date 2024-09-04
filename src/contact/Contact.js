7348b20a94e857fbef4fb2bdfbb58f9b325b9037
POOJABINJWA <poojasureshbinjwa21@navgurukul.org>
2024-09-04 15:28:37 +0530

    contact page respnsive done

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -3,13 +3,13 @@ import  DonationContactForm from "../ContactUsForm";
 function Contact (){
     return(
         <div className="contactContainer">
-            <img src="conatc.avif" alt=""/>
+            <img src="conatc.avif" alt="" className="aboutImage"/>
             <h4 className="conatctHeader">Together, We Can Create a Brighter Future .</h4>
 
             <div className="rating">
                 <div className="succes">
                     <div className="image_box">
-                        <img src="success.png"/>
+                        <img src="success.png" className="image_1"/>
                     </div>
                     <div className="rating_box">
                         <h3 className="p_tag">99k</h3>
@@ -21,7 +21,7 @@ function Contact (){
 
                 <div className="succes">
                     <div className="image_box"> 
-                        <img src="succes_2.png"/>
+                        <img src="succes_2.png" className="image_1"/>
                     </div>
                     <div className="rating_box">
                         <h3 className="p_tag">5k</h3>
@@ -33,7 +33,7 @@ function Contact (){
 
                 <div className="succes">
                     <div className="image_box">
-                        <img src="/succes_3.png"/>
+                        <img src="/succes_3.png" className="image_1"/>
                     </div>
                     <div className="rating_box">
                         <h3 className="p_tag">25+</h3>
@@ -46,7 +46,7 @@ function Contact (){
 
                 <div className="succes">
                     <div className="image_box">
-                        <img src="succes_4.png"/>
+                        <img src="succes_4.png" className="image_1"/>
                     </div>
                     <div className="rating_box">
                         <h3 className="p_tag">260+</h3>
