da5ae9c83985d66c85b1e86904b8742493ff6f15
POOJABINJWA <poojasureshbinjwa21@navgurukul.org>
2024-09-05 09:59:10 +0530

    home page style changes

--- src/Donair_details_col.js
+++ src/Donair_details_col.js
@@ -15,10 +15,10 @@ function Donair_details_col({ details }) {
       <div className="fetchdataDetails">
         
         <p className="title">{details.title}</p>
-        <p className="details">{details.description}</p>
-      </div>
-      <div className="addCartDetails">
+     
         <p className="dolor"><span className="dolarshap">Rs</span> <span className="money">{details.amount}</span></p>
+
+        <p className="details">{details.description}</p>
       </div>
       <button className="add-to-cart" onClick={handleAddToCart}>+ Add to cart</button>
     </div>
